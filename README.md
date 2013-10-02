c793b9c62dedf17b9a49b76abeb28c0a13cd6549
tea2code <stefan4875@gmail.com>
2013-10-02 19:04:15 +0200

    Added favicons.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Recipe Manager
 
 A simple Python and SQLite based recipe manager.
+
+## Other work
+
+- [Favicons by Aha-Soft](http://www.aha-soft.com/free-icons/free-blue-cloud-icons/)
--- recipe_manager.py
+++ recipe_manager.py
@@ -13,14 +13,15 @@ def css(file):
     """ Static content: css. """
     return static_file(file, root='static/css')
 
+@route('/<file:re:(favicon|apple-touch-icon)\.(png|ico)>')
 @route('/img/<file>')
 def img(file):
-    """ Static content: css. """
+    """ Static content: img. """
     return static_file(file, root='static/img')
 
 @route('/js/<file>')
 def js(file):
-    """ Static content: css. """
+    """ Static content: js. """
     return static_file(file, root='static/js')
 
 # Run ##########################################################################
