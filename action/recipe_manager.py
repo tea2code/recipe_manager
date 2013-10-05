e84f7ebcde6910c6f9a0a3cb400de7bce1f8e949
tea2code <stefan4875@gmail.com>
2013-10-05 15:29:15 +0200

    Added cookie based mechanism to use hints in recipe manager.

--- action/recipe_manager.py
+++ action/recipe_manager.py
@@ -1,5 +1,6 @@
 from bottle import redirect
 from bottle import request
+from bottle import response
 from entity import category as category_entity
 from entity import recipe
 from helper import hint
@@ -7,10 +8,23 @@ from helper import hint
 class RecipeManager:
     """ Handle recipe related actions.
 
+    Constants:
+    HINT_COOKIE -- Name of hint cookie (string).
+    HINT_DELETE -- Value of delete hint cookie (string).
+    HINT_EDIT -- Value of edit hint cookie (string).
+    HINT_NAME -- Name of cookie which stores name of last changed recipe (string).
+    HINT_NEW -- Value of new hint cookie (string).
+
     Member:
     db -- The database connection.
-    hints -- List of hints which occurred during action handling.
+    hints -- List of hints which occurred during action handling (list).
     """
+
+    HINT_COOKIE = 'show_hint'
+    HINT_DELETE = 'delete'
+    HINT_EDIT = 'edit'
+    HINT_NAME = 'last_name'
+    HINT_NEW = 'new'
 
     def __init__(self, db):
         self.db = db
@@ -22,6 +36,8 @@ class RecipeManager:
         is_new = id is None
         is_edit = request.forms.get('edit') is not None
         is_delete = request.forms.get('delete') is not None
+
+        # Actions
         if is_edit:
             category_id = int(request.forms.get('category'))
             category = category_entity.Category.find_pk(self.db, category_id)
@@ -35,13 +51,43 @@ class RecipeManager:
             result.serving_size = request.forms.get('serving-size')
             result.title = request.forms.get('title')
             result.save(self.db)
+
+            type = self.HINT_NEW if is_new else self.HINT_EDIT
+            self.__set_cookie(self.HINT_COOKIE, type)
+            self.__set_cookie(self.HINT_NAME, result.title)
             redirect('/manage/recipe/'+str(result.id))
         elif is_delete:
             old_recipe = recipe.Recipe.find_pk(self.db, id)
             old_recipe.delete(self.db)
+
+            self.__set_cookie(self.HINT_COOKIE, self.HINT_DELETE)
+            self.__set_cookie(self.HINT_NAME, old_recipe.title)
             redirect('/manage/recipe')
         elif is_new:
             result = recipe.Recipe()
         else:
             result = recipe.Recipe.find_pk(self.db, id)
+
+        # Cookies
+        hint_cookie = request.get_cookie(self.HINT_COOKIE)
+        name_cookie = request.get_cookie(self.HINT_NAME)
+        if hint_cookie and name_cookie:
+            if hint_cookie == self.HINT_NEW:
+                hint_text = 'New recipe "{}" created.'.format(name_cookie)
+            elif hint_cookie == self.HINT_EDIT:
+                hint_text = 'Recipe "{}" updated.'.format(name_cookie)
+            else:
+                hint_text = 'Recipe "{}" removed.'.format(name_cookie)
+            self.hints.append(hint.Hint(hint_text))
+            self.__delete_cookie(self.HINT_COOKIE)
+            self.__delete_cookie(self.HINT_NAME)
+
         return result
+
+    def __delete_cookie(self, name,):
+        """ Helper to delete all cookies with same settings. """
+        response.delete_cookie(name, httponly=True, path='/')
+
+    def __set_cookie(self, name, value):
+        """ Helper to set all cookies with same settings. """
+        response.set_cookie(name, value, httponly=True, path='/')
