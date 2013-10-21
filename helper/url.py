ef032b74299cb09532d5a41af1ae05b31e774fcb
tea2code <stefan4875@gmail.com>
2013-10-21 18:25:57 +0200

    Common url generation class implemented.

---
+++ helper/url.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import urllib
+
+class Url:
+    """ Generates URLs. """
+
+    @staticmethod
+    def from_category(category, absolute=None):
+        """ Generates a URL from a category. Returns relative URL string. If
+        absolute is set to the domain the returned URL will be absolute. """
+        path_parts = ['category',
+                      '{}-{}'.format(category.id, category.name.lower())]
+        return Url.from_path(path_parts, absolute)
+
+    @staticmethod
+    def from_css(css_file, absolute=None):
+        """ Generates a URL from a css file name. Returns relative URL string.
+        If absolute is set to the domain the returned URL will be absolute. """
+        return Url.from_path(['css', css_file], absolute)
+
+    @staticmethod
+    def from_js(js_file, absolute=None):
+        """ Generates a URL from a css file name. Returns relative URL string.
+        If absolute is set to the domain the returned URL will be absolute. """
+        return Url.from_path(['js', js_file], absolute)
+
+    @staticmethod
+    def from_path(path_parts, absolute=None):
+        """ Generates a URL from a list of path parts. Returns relative
+        URL string. If absolute is set to the domain the returned URL will
+        be absolute. """
+        url = '/'.join(urllib.parse.quote(p) for p in path_parts)
+        url = '/' + url
+        if absolute:
+            url = absolute + url
+        return url
+
+    @staticmethod
+    def from_recipe(recipe, absolute=None):
+        """ Generates a URL from a recipe. Returns relative URL string. If
+        absolute is set to the domain the returned URL will be absolute. """
+        path_parts = ['recipe',
+                      '{}-{}'.format(recipe.id, recipe.title.lower())]
+        return Url.from_path(path_parts, absolute)
--- static/js/main.js
+++ static/js/main.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
     {
         var image_count = parseInt($('#new-image-counter').val());
         $('#new-image-counter').val(image_count + 1);
-        var input = '<div>' +
+        var input = '<div class="input-row">' +
                     '    <label for="new-image-'+image_count+'">New Image '+(image_count+1)+':</label>' +
                     '    <input type="file" id="new-image-'+image_count+'" accept="image/gif, image/jpeg, image/png" name="new-image-'+image_count+'" />' +
                     '</div>';
@@ -36,7 +36,7 @@ $(document).ready(function() {
     {
         var synonym_count = parseInt($('#synonym-counter').val());
         $('#synonym-counter').val(synonym_count + 1);
-        var input = '<div>' +
+        var input = '<div class="input-row">' +
                     '    <label for="synonym-'+synonym_count+'">Synonym '+(synonym_count+1)+':</label>' +
                     '    <input type="text" id="synonym-'+synonym_count+'" name="synonym-'+synonym_count+'" />' +
                     '</div>';
@@ -49,11 +49,11 @@ $(document).ready(function() {
     {
         var url_count = parseInt($('#url-counter').val());
         $('#url-counter').val(url_count + 1);
-        var input = '<div>' +
+        var input = '<div class="input-row">' +
                     '    <label for="url-name-'+url_count+'">Name of Link '+(url_count+1)+':</label>' +
                     '    <input type="text" id="url-name-'+url_count+'" name="url-name-'+url_count+'" />' +
                     '</div>' +
-                    '<div>' +
+                    '<div class="input-row">' +
                     '    <label for="url-url-'+url_count+'">Link '+(url_count+1)+':</label>' +
                     '    <input type="text" id="url-url-'+url_count+'" name="url-url-'+url_count+'" />' +
                     '</div>';
