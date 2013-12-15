590e5a893b20d0ac6adfe791c86392aa1a0c99c7
tea2code <stefan4875@gmail.com>
2013-12-15 19:29:01 +0100

    Encode image paths.

--- helper/url.py
+++ helper/url.py
@@ -21,6 +21,15 @@ class Url:
         return Url.from_path(['css', css_file], absolute)
 
     @staticmethod
+    def from_img(img_file, absolute=None):
+        """ Generates a URL from a image file name. Returns relative URL string.
+        If absolute is set to the domain the returned URL will be absolute. """
+        url = urllib.parse.quote(img_file)
+        if absolute:
+            url = absolute + url
+        return url
+
+    @staticmethod
     def from_js(js_file, absolute=None):
         """ Generates a URL from a css file name. Returns relative URL string.
         If absolute is set to the domain the returned URL will be absolute. """
